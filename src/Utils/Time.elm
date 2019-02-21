e5b6ee3eab8ffefd9be9a61ed55946fb8aeb9a9a
Panagiotis Georgiadis <pgeorgiadis@travelrepublic.co.uk>
2019-02-21 18:02:40 +0000

    Did some housekeeping on the Time Utils file.

--- src/Utils/Time.elm
+++ src/Utils/Time.elm
@@ -4,61 +4,59 @@ module Utils.Time exposing
     , precedingWeekdays
     , timeToString
     , toHumanReadableDate
-    , toHumanReadableTime
+    , toHumanReadableDateTime
     , weekdayToString
     , weekdayToStringCondensed
-    , zeroTime
     )
 
 import DateTime exposing (DateTime)
 import Time exposing (Month(..), Posix, Weekday(..), Zone)
 
 
-zeroTime : Posix
-zeroTime =
-    Time.millisToPosix 0
-
-
-toHumanReadableTime : Zone -> Posix -> String
-toHumanReadableTime zone time =
+toHumanReadableDateTime : DateTime -> String
+toHumanReadableDateTime dateTime =
     let
-        ( weekday, month ) =
-            ( Time.toWeekday zone time
-            , Time.toMonth zone time
-            )
-
-        ( day, year, experiment ) =
-            ( Time.toDay zone time
-            , Time.toYear zone time
-            , 0
-            )
-
         humanReadableTime =
             String.join ":"
-                [ timeToString (Time.toHour zone time)
-                , timeToString (Time.toMinute zone time)
-                , timeToString (Time.toSecond zone time)
+                [ timeToString (DateTime.getHours dateTime)
+                , timeToString (DateTime.getMinutes dateTime)
                 ]
     in
     String.join " "
-        [ weekdayToStringCondensed weekday
-        , monthToStringCondensed month
-        , String.fromInt day
-        , String.fromInt year
+        [ toHumanReadableDate dateTime
         , humanReadableTime
+        ]
+
+
+toHumanReadableDate : DateTime -> String
+toHumanReadableDate dateTime =
+    String.join " "
+        [ weekdayToStringCondensed (DateTime.getWeekday dateTime)
+        , String.fromInt (DateTime.getDay dateTime)
+        , monthToStringCondensed (DateTime.getMonth dateTime)
+        , String.fromInt (DateTime.getYear dateTime)
         ]
 
 
 timeToString : Int -> String
 timeToString time =
-    if time == 0 then
-        "00"
-
-    else if time < 10 then
+    if time < 10 then
         "0" ++ String.fromInt time
 
     else
         String.fromInt time
+
+
+millisToString : Int -> String
+millisToString millis =
+    if millis < 10 then
+        "00" ++ String.fromInt millis
+
+    else if millis < 100 then
+        "0" ++ String.fromInt millis
+
+    else
+        String.fromInt millis
 
 
 weekdayToString : Weekday -> String
@@ -136,86 +134,6 @@ monthToStringCondensed =
     String.left 3 << monthToString
 
 
-
--- monthToInt : Month -> Int
--- monthToInt month =
---     case month of
---         Jan ->
---             1
---         Feb ->
---             2
---         Mar ->
---             3
---         Apr ->
---             4
---         May ->
---             5
---         Jun ->
---             6
---         Jul ->
---             7
---         Aug ->
---             8
---         Sep ->
---             9
---         Oct ->
---             10
---         Nov ->
---             11
---         Dec ->
---             12
---
---
--- monthFromInt : Int -> Maybe Month
--- monthFromInt monthInt =
---     case monthInt of
---         1 -> Just Jan
---         2 -> Just Feb
---         3 -> Just Mar
---         4 -> Just Apr
---         5 -> Just May
---         6 -> Just Jun
---         7 -> Just Jul
---         8 -> Just Aug
---         9 -> Just Sep
---         10 -> Just Oct
---         11 -> Just Nov
---         12 -> Just Dec
---         _ -> Nothing
--- getPreviousMonth : Month -> Month
--- getPreviousMonth month =
---     case month of
---         Jan -> Dec
---         Feb -> Jan
---         Mar -> Feb
---         Apr -> Mar
---         May -> Apr
---         Jun -> May
---         Jul -> Jun
---         Aug -> Jul
---         Sep -> Aug
---         Oct -> Sep
---         Nov -> Oct
---         Dec -> Nov
---
---
--- getNextMonth : Month -> Month
--- getNextMonth month =
---     case month of
---         Jan -> Feb
---         Feb -> Mar
---         Mar -> Apr
---         Apr -> May
---         May -> Jun
---         Jun -> Jul
---         Jul -> Aug
---         Aug -> Sep
---         Sep -> Oct
---         Oct -> Nov
---         Nov -> Dec
---         Dec -> Jan
-
-
 precedingWeekdays : Weekday -> Int
 precedingWeekdays weekday =
     case weekday of
@@ -239,17 +157,3 @@ precedingWeekdays weekday =
 
         Sat ->
             6
-
-
-
-{- We'll see -}
-
-
-toHumanReadableDate : DateTime -> String
-toHumanReadableDate date =
-    String.join " "
-        [ weekdayToString (DateTime.getWeekday date)
-        , String.fromInt (DateTime.getDay date)
-        , monthToStringCondensed (DateTime.getMonth date)
-        , String.fromInt (DateTime.getYear date)
-        ]
