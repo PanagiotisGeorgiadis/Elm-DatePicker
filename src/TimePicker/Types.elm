09f45bb25f771cd923a014322b71af6a82d6f9b0
Panagiotis Georgiadis <pgeorgiadis@travelrepublic.co.uk>
2019-03-14 18:02:12 +0000

    Added more documentation on the TimePicker.Types

--- src/TimePicker/Types.elm
+++ src/TimePicker/Types.elm
@@ -1,14 +1,30 @@
 module TimePicker.Types exposing (PickerType(..))
 
-{-| This module contains the `Public types` that can be used in order to create a TimePickerConfig
-for either the `DatePicker` or the `DateRangePicker` modules.
+{-| This module contains the `Public types` that are being used by the
+_**parent application**_ in order to create a `TimePickerConfig` for either
+the `DatePicker` or the `DateRangePicker` modules.
+
+
+# Types
 
 @docs PickerType
 
 -}
 
 
-{-| Describes different picker types.
+{-| Describes the different _**picker types**_ along with their _**stepping function**_.
+
+    -- An `Hours` and `Minutes` picker. The `Hours` part
+    -- will increment / decrement their value by 1 whereas
+    -- the `Minutes` will increment / decrement by 5.
+    HH_MM { hoursStep = 1, minutesStep = 5 }
+
+    -- An `Hours`, `Minutes` and `Seconds` picker.
+    -- The `Hours` and `Minutes` will increment / decrement
+    -- their values by 1 whereas the `Seconds` will
+    -- increment / decrement by 10.
+    HH_MM_SS { hoursStep = 1, minutesStep = 1, secondsStep = 10 }
+
 -}
 type PickerType
     = HH { hoursStep : Int }
